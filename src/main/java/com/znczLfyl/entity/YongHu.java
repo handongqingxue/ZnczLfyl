6f470d66de2686da31f5bc6e48f83e078e070c82
handongqingxue <1417140290@qq.com>
2021-10-29 14:54:30 +0800

    完善

---
+++ src/main/java/com/znczLfyl/entity/YongHu.java
@@ -0,0 +1,68 @@
+package com.znczLfyl.entity;
+
+public class YongHu {
+
+	private Integer id;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getYhm() {
+		return yhm;
+	}
+	public void setYhm(String yhm) {
+		this.yhm = yhm;
+	}
+	public String getMm() {
+		return mm;
+	}
+	public void setMm(String mm) {
+		this.mm = mm;
+	}
+	public String getNc() {
+		return nc;
+	}
+	public void setNc(String nc) {
+		this.nc = nc;
+	}
+	public String getTx() {
+		return tx;
+	}
+	public void setTx(String tx) {
+		this.tx = tx;
+	}
+	public String getSm() {
+		return sm;
+	}
+	public void setSm(String sm) {
+		this.sm = sm;
+	}
+	public Integer getZt() {
+		return zt;
+	}
+	public void setZt(Integer zt) {
+		this.zt = zt;
+	}
+	public String getJs() {
+		return js;
+	}
+	public void setJs(String js) {
+		this.js = js;
+	}
+	public Integer getJsid() {
+		return jsid;
+	}
+	public void setJsid(Integer jsid) {
+		this.jsid = jsid;
+	}
+	private String yhm;
+	private String mm;
+	private String nc;
+	private String tx;
+	private String sm;
+	private Integer zt;
+	private String js;
+	private Integer jsid;
+}
